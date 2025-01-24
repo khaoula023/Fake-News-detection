f576407955440fff44f3670d1e04bf5bae7dd79b
khaoula023 <khaouladahimi02@gmail.com>
2025-01-24 21:18:39 +0100

    Data Ingestion

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,21 @@ def load_object(file_path):
             return dill.load(file)
     except Exception as e:
         raise CustomException(e, sys)
+    
+def balance(data):
+    try:
+        # Split the dataset by label
+        df_minority = data[data['label'] == 0]  # Minority class (label=0)
+        df_majority = data[data['label'] == 1]  # Majority class (label=1)
+
+        # Perform undersampling (reduce majority class to the size of minority class)
+        df_majority_undersampled = df_majority.sample(n=len(df_minority), random_state=42)
+
+        # Combine the undersampled majority class with the minority class
+        df_balanced = pd.concat([df_minority, df_majority_undersampled])
+
+        # Shuffle the dataset (optional, but recommended for randomization)
+        df = df_balanced.sample(frac=1, random_state=42).reset_index(drop=True)
+        return df
+    except Exception as e:
+        raise CustomException(e, sys)
