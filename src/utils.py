6477f5dafdb3f0de8d9717f8df992b2e72ef757d
khaoula023 <khaouladahimi02@gmail.com>
2025-01-24 20:44:50 +0100

    Exception and Logger

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import dill
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file:
+            dill.dump(obj, file)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e, sys)
